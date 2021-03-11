a02804108baebeceed4a1d8c44bca228f0752fc2
Quentin Quarantino <cedricknzumstein@gmail.com>
2021-03-11 23:03:03 +0100

    modified:   saveconfsdeb.sh

--- saveconfsdeb.sh
+++ saveconfsdeb.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 
+minus="--------------------------------------------------------------------------------"
+D=$(date +'%d')
+if [ $USER != root ] ; then
+        printf "\nyou must be root; exit script!\n$minus\n\n"
+        exit 1
+fi
+
+
+#fehlend Ordner werden erstellt
+for i in $(seq 1 31) ; do i=`echo 0$i |tail -c 3` ; [[ -d /saveconf/date$i/{log,net,block,conf} ]] || mkdir -p /saveconf/date$i/{log,net,block,config} ;done
+
+echo $(date) >/saveconf/date$D/df-hP ;df-hP >>/saveconf/date$D/df-hP
+echo $(date) >/saveconf/date$D/dmesg :dmesg -T >>/saveconf/date$D/dmesg
+[ -d /saveconf/date$i/ ] ||  mkdir -p /saveconf/date$i
