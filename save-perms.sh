7cf1f56185d9a79a927fa22ade095497a0fd48b2
Quentin Quarantino <cedricknzumstein@gmail.com>
2021-03-24 20:08:48 +0100

    modified:   save-perms.sh

--- save-perms.sh
+++ save-perms.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 RestoreScript="`echo $1 |sed -e 's/\//_/g'`.`date +%Y%m%d`" 
 minus="--------------------------------------------------------------------------------"
+if [ $USER != root ] ; then
+        printf "\nyou must be root; exit script!\n$minus\n\n"
+        exit 1
+fi
 usage() 
 {
 	exit 1
@@ -14,7 +18,8 @@ check()
 save()
 {
 	echo '#!/bin/bash' >${RestoreScript}
-	echo "restore script generated form $USER at $(date) for all files and folders from $1"
+	echo "restore script generated form $USER at $(date) for all files and folders from $1" >>${RestoreScript}
+	echo 'if [ $USER != root ] ; then echo "you must be root; exit script" && exit 1 ; fi' >>${RestoreScript}
 	find $1 -ignore_readdir_race -perm /7777 2>/dev/null -exec stat --printf=' f="%n" ; [ -e "$f" ] && [ "\`stat --printf=\%a "$f" 2>/dev/null\`" -ne %a ] && chmod %a "$f" && echo $f   permission corrected to %a \n' {} \;  >>${RestoreScript} 
 }
 
