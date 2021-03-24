0504ccdc0b6bffca6974008821c43aa139af4641
Quentin Quarantino <cedricknzumstein@gmail.com>
2021-03-24 20:00:09 +0100

    modified:   save-perms.sh

--- save-perms.sh
+++ save-perms.sh
@@ -18,4 +18,9 @@ save()
 	find $1 -ignore_readdir_race -perm /7777 2>/dev/null -exec stat --printf=' f="%n" ; [ -e "$f" ] && [ "\`stat --printf=\%a "$f" 2>/dev/null\`" -ne %a ] && chmod %a "$f" && echo $f   permission corrected to %a \n' {} \;  >>${RestoreScript} 
 }
 
+## --------------------------------------------------------------------------------
+# main
+
+#save
+
 exit 1
